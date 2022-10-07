7a69294c475418bd60f109143635c8e87ae0d899
osoroderrick <osoroderrick@yahoo.com>
2022-10-06 20:19:50 -0400

    Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Collaborative pushing practice");
         System.out.println("-Tayyaba");
+
+        System.out.println("Derrick was here");
+        System.out.println("Derrick O");
     }
 }
